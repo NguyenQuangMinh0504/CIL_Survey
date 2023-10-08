47b3cf38deee4ec9ce3a4637e38c30f40718ceab
Nguyen Quang Minh <ngquangminh05042001@gmail.com>
2023-10-08 16:37:26 +0700

    Update base.py

--- models/base.py
+++ models/base.py
@@ -8,6 +8,9 @@ from utils.toolkit import tensor2numpy, accuracy
 from scipy.spatial.distance import cdist
 import os
 
+torch.backends.cudnn.enabled = False
+torch.cuda.is_available = lambda: False
+
 EPSILON = 1e-8
 batch_size = 64
 
