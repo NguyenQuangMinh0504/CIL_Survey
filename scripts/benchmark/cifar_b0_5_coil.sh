bdbcefdfe676c49f36784069999d59ed7ef74399
Nguyen Quang Minh <ngquangminh05042001@gmail.com>
2023-10-16 14:08:32 +0700

    Update cifar_b0_5_coil.sh

--- scripts/benchmark/cifar_b0_5_coil.sh
+++ scripts/benchmark/cifar_b0_5_coil.sh
@@ -1,7 +1,7 @@
-CUDA_VISIBLE_DEVICES=1 python main.py \
+CUDA_VISIBLE_DEVICES=0 python main.py \
     -model coil \
     -init 5 \
     -incre 5 \
     -net cosine_resnet32 \
     -p benchmark \
-    -d 0
+    -d -1
